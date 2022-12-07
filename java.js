74afec60b50bae18fe7886ebb6395257f3803d15
PSNaveen1511 <120000844+PSNaveen1511@users.noreply.github.com>
2022-12-07 18:45:21 +0530

    java

---
+++ java.js
@@ -0,0 +1,20 @@
+// COVID-19 Ticket Booking Code//
+const row = 5;
+const colm = 5;
+myarray = []
+v = 0;
+names = ['cherath', 'esakki', 'gayathiri', 'suriya', 'babu bro', 'AJ bro', 'AAA']
+for (i = 0; i < row; i++) {
+    myarray[i] = [];
+    for (j = 0; j < colm; j++) {
+        if (j % 2 == 0 < i % 2 == 0) {
+            myarray[i][j] = "covid:-(19)";
+        }  else if (names[v]==null) {
+            myarray[i][j] = 1;
+        }else {
+            myarray[i][j] = names[v++];
+
+        }
+    }
+
+}; console.log(myarray)
